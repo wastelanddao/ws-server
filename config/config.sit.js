2c4f0d223a329b48cebff51c2c1ea39b6fc7ee91
worker88 <work88@wasteland.com>
2022-05-13 14:46:10 +0800

    集成测试环境配置

---
+++ config/config.sit.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // add your user config here
+  const userConfig = {
+    moralis: {
+      serverUrl: 'https://9jb2mu5k9rwl.usemoralis.com:2053/server',
+      appId: 'Acp7Y2B66otKg9pxcvCsoIpd8fnCr0gulX6l6l8p',
+      masterKey: 'wMqzIe4K1XvmLdiO98e2Ai0CYpfPmQxiBJh0adxk',
+    },
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
